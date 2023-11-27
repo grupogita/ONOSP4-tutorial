ecd42e74bf89b43ec4a4a5aef768db8d118f3fed
sgutierb <113127823+sgutierb@users.noreply.github.com>
2023-11-27 07:24:46 -0500

    Update topo.py

--- Exercise-2/mininet/topo.py
+++ Exercise-2/mininet/topo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python
 
 import argparse
 
@@ -14,47 +14,51 @@ from mininet.node import CPULimitedHost
 
 import sys
 
-sys.path.insert(0, '~/ngsdn-tutorial/mininet')
-from stratum2 import StratumBmv2Switch
+sys.path.insert(0, '~/ONOS-P4TUTORIAL/Exercise-2/mininet')
+from stratum_new import StratumBmv2Switch
 
 CPU_PORT = 255
 
 
 class TutorialTopo(Topo):
-    """2x2 fabric topology with IPv4 hosts"""
-
     def __init__(self, *args, **kwargs):
         Topo.__init__(self, *args, **kwargs)
 
-        print(self)
+        #Switch
+	""" Exercise 1 TO-DO
+ 	Set the path to the json file which is output of the P4 compilation step
+  	"""
+        s1 = self.addSwitch('s1', cls=StratumBmv2Switch, cpuport=CPU_PORT, onosdevid="1", json="<TODO>")
 
-        # Leaves
 
-        # gRPC port 50001
-        s1 = self.addSwitch('s1', cls=StratumBmv2Switch, cpuport=CPU_PORT, onosdevid="1", json="../p4src/main.json")
+        """
+        Exercise 1 TO-DO:
+        Add two hosts and create links between the SW and them. h1 should have
+        the mac address 00:00:00:00:00:01 and IP 10.0.0.1/24. For h2 use
+        00:00:00:00:00:02 and 10.0.0.2/24 for the mac address and ip addres respectively.
+        Besides, the first link (h1-s1) should have the following specifications:
+                        Bandwidth of 2 Mbps, delay of 10000us and loss of 5%.
+        The second link (h2-s2) should have the following specifications:
+                        Bandwidth of 5 Mbps, delay of 1ms and loss of 2%.
 
-        # IPv4 hosts attached to S1
-        h1 = self.addHost('h1', mac="00:00:00:00:00:01", ip='10.0.0.1/24') #, gw='10.0.0.254')
-        self.addLink(s1, h1)   #Host1  #Port 1
 
-        # IPv4 hosts attached to S2
-        h2 = self.addHost('h2', mac="00:00:00:00:00:02", ip='10.0.0.2/24') #, gw='10.0.0.254')
-        self.addLink(s1, h2)  #Port 2
+                        HINT: search on google -> self.addlink use_htb=True
+        """
 
 
 def main():
-        net = Mininet(topo=TutorialTopo(), controller=None, link=TCLink)#, switch=OVSKernelSwitch)
-        net.staticArp() #Avoiding ARP process
-        net.start()
-        CLI(net)
-        net.stop()
-        print('#' * 80)
-        print('ATTENTION: Mininet was stopped! Perhaps accidentally?')
-        print('No worries, it will restart automatically in a few seconds...')
-        print('To access again the Mininet CLI, use `make mn-cli`')
-        print('To detach from the CLI (without stopping), press Ctrl-D')
-        print('To permanently quit Mininet, use `make stop`')
-        print('#' * 80)
+	net = Mininet(topo=TutorialTopo(), controller=None, link=TCLink)
+	net.staticArp() #Avoid ARP process
+	net.start()
+	CLI(net)
+	net.stop()
+	print '#' * 80
+	print 'ATTENTION: Mininet was stopped! Perhaps accidentally?'
+	print 'No worries, it will restart automatically in a few seconds...'
+	print 'To access again the Mininet CLI, use `make mn-cli`'
+	print 'To detach from the CLI (without stopping), press Ctrl-D'
+	print 'To permanently quit Mininet, use `make stop`'
+	print '#' * 80
 
 
 if __name__ == "__main__":
